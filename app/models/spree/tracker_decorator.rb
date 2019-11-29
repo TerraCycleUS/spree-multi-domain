a9c111dd8aeb90672018e9ef1b87ec92a5c4778d
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-11-29 10:24:22 +0100

    Corrects model decorators

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,10 +1,16 @@
-if defined?(Spree::Tracker)
-  Spree::Tracker.class_eval do
-    belongs_to :store
+module Spree
+  module TrackerDecorator
+    def self.prepended(base)
+      base.belongs_to :store
+      base.extend ClassMethods
+    end
 
-    def self.current(engine = Spree::Tracker::TRACKING_ENGINES, domain)
-      Spree::Tracker.where(engine: engine).active.joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
+    module ClassMethods
+      def find_current(domain, engine = Spree::Tracker::TRACKING_ENGINES)
+        Spree::Tracker.where(engine: engine).active.joins(:store).where("spree_stores.url LIKE ?", "%#{domain}%").first
+      end
     end
   end
 end
 
+::Spree::Tracker.prepend(Spree::TrackerDecorator) if defined?(Spree::Tracker)
